33bc0d8908f04985fe91b5e39a510efb51570852
Nazrawi Demeke <dnazrawi@gmail.com>
2021-12-10 16:48:30 +0300

    more week 1 submissions

---
+++ week_1/selection-sort.py
@@ -0,0 +1,9 @@
+class Solution:
+    def select(self, arr_trunc, i):
+        return arr_trunc.index(min(arr_trunc)) + i
+
+    def selectionSort(self,arr,n):
+        for i in range(n):
+            selected = self.select(arr[i:],i)
+            arr[i], arr[selected] = arr[selected], arr[i]
+        return arr
