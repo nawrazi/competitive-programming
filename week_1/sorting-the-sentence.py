c30003933752d785da3e6c6b2e5f76f8ee450af3
Nazrawi Demeke <dnazrawi@gmail.com>
2021-12-10 16:57:23 +0300

    more week 1 submissions

---
+++ week_1/sorting-the-sentence.py
@@ -0,0 +1,11 @@
+# https://leetcode.com/problems/sorting-the-sentence/submissions/
+
+class Solution:
+    def sortSentence(self, s: str) -> str:
+        words = s.split()
+        sent_list = [None for _ in range(len(words))]
+        for word in words:
+            index = int(word[-1]) - 1
+            sent_list[index] = word[:-1]
+
+        return ' '.join(sent_list)
