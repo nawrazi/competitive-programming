9b5fc9bccc79ebcc477e26b1e08fe386120f1ec4
Nazrawi Demeke <dnazrawi@gmail.com>
2021-12-10 16:21:21 +0300

    more week 1 submissions

---
+++ week_1/counting-sort-1.py
@@ -0,0 +1,32 @@
+# https://www.hackerrank.com/challenges/countingsort1/problem
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'countingSort' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+def countingSort(arr):
+    # Write your code here
+    list = [0 for _ in range(100)]
+
+    for num in arr:
+        list[num]+=1
+
+    return list
+
+if __name__ == '__main__':
+    n = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = countingSort(arr)
+
+    print(*result)
