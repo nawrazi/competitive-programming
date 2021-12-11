0abf5f69dfc96cdfd1c481b5e5123ca13dc75726
Nazrawi Demeke <dnazrawi@gmail.com>
2021-12-11 14:22:57 +0300

    more week 1 submissions

---
+++ week_1/merge-intervals.py
@@ -0,0 +1,16 @@
+# https://leetcode.com/problems/merge-intervals/submissions/
+
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        n = len(intervals)
+        intervals.sort()
+        i=0
+        while i<n-1:
+            if intervals[i][1]>=intervals[i+1][0]:
+                intervals[i]=[intervals[i][0], max(intervals[i+1][1],intervals[i][1])]
+                intervals.pop(i+1)
+                n-=1
+                continue
+            i+=1
+
+        return intervals
