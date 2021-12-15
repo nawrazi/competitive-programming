9ea46c59d5237e16e07208b78fae3fab142de214
Nazrawi Demeke <dnazrawi@gmail.com>
2021-12-15 18:37:16 +0300

    more week 2 submissions

---
+++ week_2/frequency-of-most-frequent.py
@@ -0,0 +1,19 @@
+class Solution:
+    def maxFrequency(self, nums: List[int], k: int) -> int:
+        nums.sort(reverse=True)
+        Max = 1
+        n = len(nums)-1
+        i,j = 0,0
+
+        while j<n:
+            diff = nums[i]-nums[j+1]
+            if diff<=k:
+                j+=1
+                Max = max(Max,j-i+1)
+                k-=diff
+
+            elif diff>k:
+                k+=(nums[i]-nums[i+1])*(j-i)
+                i+=1
+
+        return Max
