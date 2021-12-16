4129c49c6d986953200b6a27d96a5e5cb91b89f5
Nazrawi Demeke <dnazrawi@gmail.com>
2021-12-16 18:57:14 +0300

    more week 2 submissions

---
+++ week_2/good-pairs.py
@@ -0,0 +1,15 @@
+# https://leetcode.com/problems/number-of-good-pairs/submissions/
+
+class Solution:
+    def numIdenticalPairs(self, nums: List[int]) -> int:
+        n = len(nums)
+        freq = [0 for _ in range(101)]
+        total = 0
+
+        for i in range(n):
+            freq[nums[i]]+=1
+
+        for i in range(101):
+            total += int(freq[i] * (freq[i]-1) * 0.5)
+
+        return total
