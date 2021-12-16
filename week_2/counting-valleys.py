150d22fcb0e76a49e21f8760a5bd12d0f1c795c4
Nazrawi Demeke <dnazrawi@gmail.com>
2021-12-16 21:30:50 +0300

    more week 2 commits

---
+++ week_2/counting-valleys.py
@@ -0,0 +1,47 @@
+# https://www.hackerrank.com/challenges/counting-valleys/problem
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'countingValleys' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER steps
+#  2. STRING path
+#
+
+def countingValleys(steps, path):
+    level = 0
+    valleys = 0
+
+    n= len(path)
+
+    for i in range(n):
+        if path[i]=='U':
+            level+=1
+
+            if level==0:
+                valleys+=1
+        else:
+            level-=1
+
+    return valleys
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    steps = int(input().strip())
+
+    path = input()
+
+    result = countingValleys(steps, path)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
