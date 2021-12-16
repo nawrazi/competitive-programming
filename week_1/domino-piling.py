f6065b5743d0ececa8f187097fea544bdd6c12f1
Nazrawi Demeke <dnazrawi@gmail.com>
2021-12-16 12:15:08 +0300

    refactor

--- week_1/domino-piling.py
+++ week_1/domino-piling.py
@@ -1,15 +1,4 @@
 # https://codeforces.com/problemset/problem/50/A
 
 (m,n) = [int(n) for n in input().split()]
-
-if m%2!=0 and n%2!=0:
-    dominoes = ((m-1)//2)*n + (n-1)//2
-    print(dominoes)
-
-elif m%2==0:
-    dominoes = (m//2)*n
-    print(dominoes)
-
-elif n%2==0:
-    dominoes = (n//2)*m
-    print(dominoes)
+print(m*n//2)
