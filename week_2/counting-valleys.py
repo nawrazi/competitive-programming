fa9002a9789daae817323dd9255a363047e7ece3
Nazrawi Demeke <dnazrawi@gmail.com>
2021-12-19 00:13:23 +0300

    more week 2 submissions

--- week_2/counting-valleys.py
+++ week_2/counting-valleys.py
@@ -6,15 +6,6 @@ import random
 import re
 import sys
 
-#
-# Complete the 'countingValleys' function below.
-#
-# The function is expected to return an INTEGER.
-# The function accepts following parameters:
-#  1. INTEGER steps
-#  2. STRING path
-#
-
 def countingValleys(steps, path):
     level = 0
     valleys = 0
---
+++ week_2/number-of-recent-calls.py
@@ -0,0 +1,15 @@
+# https://leetcode.com/problems/number-of-recent-calls/submissions/
+
+class RecentCounter:
+
+    def __init__(self):
+        self.counter = 0
+        self.pings = []
+
+    def ping(self, t: int) -> int:
+        self.pings.append(t)
+
+        while self.pings[0]<t-3000:
+            self.pings.pop(0)
+
+        return len(self.pings)
