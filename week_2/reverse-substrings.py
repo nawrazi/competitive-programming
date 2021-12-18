0e0aa9df155425a922282a628f9beb36079154c3
Nazrawi Demeke <dnazrawi@gmail.com>
2021-12-18 23:35:45 +0300

    more week 2 submissions

--- week_2/reverse-substrings.py
+++ week_2/reverse-substrings.py
@@ -1,38 +1,29 @@
+# https://leetcode.com/problems/reverse-substrings-between-each-pair-of-parentheses/submissions/
+
 class Solution:
-    def myStack(self, s2: str) -> str:
-        stack = []
-        l = 0
-        i=0
-
-        while i < len(s2):
-            if s2[i]=='(':
-                val,l = self.myStack(s2[i+1:])
-                stack += val
-                i+=l
-                l+=2
-
-            elif s2[i]==')':
-                # l+=1
-                return stack[::-1], l+2
-
-            else:
-                stack.append(s2[i])
-                l+=1
-                i+=1
-
-
     def reverseParentheses(self, s: str) -> str:
-        stack2 = []
+        d = {'(':')', ')':'('}
+        s = list(s)
         i=0
         while i < len(s):
-            if s[i]=='(':
-                val,l = self.myStack(s[i+1:])
-                stack2 += val
-                print(stack2)
-                i+=l
-                continue
+            if s[i] in d.keys():
+                j=i+1
+                c=0
+                while j < len(s):
+                    if c>0:
+                        break
 
-            stack2.append(s[i])
+                    if s[j] == s[i]:
+                        c-=1
+                    elif s[j] == d[s[i]]:
+                        c+=1
+                    j+=1
+
+                s[i+1:j] = s[i+1:j][::-1]
+                s.pop(i)
+                s.pop(i)
+                i-=1
+
             i+=1
 
-        return ''.join(stack2)
+        return ''.join(s)
