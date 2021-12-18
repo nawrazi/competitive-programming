92d04701236e86dbfc57fd61ef5f28f137db1db7
Nazrawi Demeke <dnazrawi@gmail.com>
2021-12-18 20:08:05 +0300

    more week 2 submissions

---
+++ week_2/top-k-frequent-elements.py
@@ -0,0 +1,23 @@
+# https://leetcode.com/problems/top-k-frequent-elements/submissions/
+
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        dict = {}
+
+        for num in nums:
+            if num in dict:
+                dict[num] += 1
+                continue
+
+            dict[num]=1
+
+        keys = list(dict.keys())
+        vals = list(dict.values())
+
+        final = []
+        for i in range(k):
+            ind = vals.index(max(vals))
+            vals.pop(ind)
+            final.append(keys.pop(ind))
+
+        return final
