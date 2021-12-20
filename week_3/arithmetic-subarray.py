b063db0ae44af269f6043abda6d8770a4daae994
Nazrawi Demeke <dnazrawi@gmail.com>
2021-12-20 21:55:04 +0300

    more week 3 submissions

---
+++ week_3/arithmetic-subarray.py
@@ -0,0 +1,19 @@
+# https://leetcode.com/problems/arithmetic-subarrays/submissions/
+
+def checkArithmeticSubarrays(nums, l, r):
+    n = len(l)
+    final = []
+    for i in range(n):
+        sub = nums[l[i]:r[i]+1]
+        sub.sort()
+        m = len(sub)
+        arithmetic = True
+        for j in range(m-1):
+            if sub[j+1] - sub[j] == sub[1] - sub[0]:
+                continue
+            arithmetic = False
+            break
+
+        final.append(arithmetic)
+
+    return final
