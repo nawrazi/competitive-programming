4f6b6f3558b9ba2ad9b9ea91df38d16bc2d5fb3c
Nazrawi Demeke <dnazrawi@gmail.com>
2021-12-31 16:50:51 +0300

    more week 3 submissions

---
+++ week_3/find-original-array.py
@@ -0,0 +1,28 @@
+# https://leetcode.com/problems/find-original-array-from-doubled-array/submissions/
+
+class Solution:
+    def findOriginalArray(self, changed: List[int]) -> List[int]:
+        changed.sort()
+        original, references, ans = [], {}, []
+
+        if set(changed) =={0} and len(changed)%2==0:
+            return [0]*(len(changed)//2)
+
+        for num in changed:
+            if num%2==0 and num//2 in references:
+                if references[num//2]==0:
+                    if num in references:
+                        references[num]+=1
+                    else:
+                        references[num]=1
+                else:
+                    references[num//2]-=1
+                    ans.append(num//2)
+
+            else:
+                if num in references:
+                    references[num]+=1
+                else:
+                    references[num] = 1
+
+        return ans if set(references.values())=={0} else []
