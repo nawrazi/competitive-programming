b2412f713f00fdfcaca20669232e286cbfc49763
Nazrawi Demeke <dnazrawi@gmail.com>
2022-01-01 14:30:55 +0300

    more week 3 submissions

---
+++ week_3/find-kth-bit.py
@@ -0,0 +1,16 @@
+# https://leetcode.com/problems/find-kth-bit-in-nth-binary-string/submissions/
+
+class Solution:
+    def recur(self,n):
+        if n==1:
+            return '0'
+
+        prev = self.recur(n-1)
+
+        inverse = [('0' if l=='1' else '1') for l in prev]
+        reverse = ''.join(inverse[::-1])
+
+        return prev + '1' + reverse
+
+    def findKthBit(self, n: int, k: int) -> str:
+        return self.recur(n)[k-1]
