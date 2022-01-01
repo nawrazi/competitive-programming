fdbd4a3083a32c9e0a6951024412ee9c1d828d37
Nazrawi Demeke <dnazrawi@gmail.com>
2022-01-01 16:00:27 +0300

    more week 3 submissions

---
+++ week_3/kth-symbol-in-grammar.py
@@ -0,0 +1,16 @@
+# https://leetcode.com/problems/k-th-symbol-in-grammar/submissions/
+
+class Solution:
+    def kthGrammar(self, n: int, k: int) -> int:
+        half_len = (2**(n-1))//2
+        bits = {'0':'1', '1':'0'}
+
+        if n==2:
+            return '0' if k==1 else '1'
+        if n==1:
+            return '0'
+
+        if k>half_len:
+            return bits[self.kthGrammar(n-1,k-half_len)]
+        else:
+            return self.kthGrammar(n-1,k)
