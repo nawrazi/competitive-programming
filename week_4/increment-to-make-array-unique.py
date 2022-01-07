885178353a28c2f69ea7111698fd2d64baa350ac
Nazrawi Demeke <dnazrawi@gmail.com>
2022-01-07 13:53:48 +0300

    first week 4 submission

---
+++ week_4/increment-to-make-array-unique.py
@@ -0,0 +1,15 @@
+# https://leetcode.com/problems/minimum-increment-to-make-array-unique/submissions/
+
+class Solution:
+    def minIncrementForUnique(self, nums: List[int]) -> int:
+        nums.sort()
+        last_num, moves = -1, 0
+
+        for cur_num in nums:
+            if cur_num <= last_num:
+                diff = last_num - cur_num + 1
+                cur_num += diff
+                moves += diff
+            last_num = cur_num
+
+        return moves
