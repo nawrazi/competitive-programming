ed6470198b56cb20e1633470116dab21cc294377
Nazrawi Demeke <dnazrawi@gmail.com>
2022-01-12 13:58:43 +0300

    more week 4 submissions

---
+++ week_4/h-index.py
@@ -0,0 +1,15 @@
+# https://leetcode.com/problems/h-index/submissions/
+
+class Solution:
+    def hIndex(self, citations: List[int]) -> int:
+        citations.sort()
+        total = len(citations)
+        max_papers = 0
+
+        for i in range(total-1, -1, -1):
+            papers = total-i
+
+            if citations[i]>=papers:
+                max_papers = papers
+
+        return maxim
