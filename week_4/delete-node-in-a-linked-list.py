e76c332f162cd36260f85089df5a2af5da437577
Nazrawi Demeke <dnazrawi@gmail.com>
2022-01-15 14:07:27 +0300

    more week 4 submissions

---
+++ week_4/delete-node-in-a-linked-list.py
@@ -0,0 +1,15 @@
+# https://leetcode.com/problems/delete-node-in-a-linked-list/submissions/
+
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+class Solution:
+    def deleteNode(self, node):
+        while node.next:
+            node.val = node.next.val
+            if not node.next.next:
+                node.next = None
+                break
+            node = node.next
