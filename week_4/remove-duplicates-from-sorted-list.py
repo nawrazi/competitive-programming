798df2ff520561dd49cea460c76b54711aa797c8
Nazrawi Demeke <dnazrawi@gmail.com>
2022-01-15 14:49:51 +0300

    more week 4 submissions

---
+++ week_4/remove-duplicates-from-sorted-list.py
@@ -0,0 +1,18 @@
+# https://leetcode.com/problems/remove-duplicates-from-sorted-list/submissions/
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        node = head
+
+        while node and node.next:
+            while node.next and node.val == node.next.val:
+                node.next = node.next.next
+
+            node = node.next
+
+        return head
