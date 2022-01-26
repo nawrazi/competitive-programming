9416e653c66fec7927ca907bcf24b53847d99dbc
Nazrawi Demeke <dnazrawi@gmail.com>
2022-01-26 19:44:11 +0300

    more week 5 submissions

---
+++ week_5/move-zeroes.py
@@ -0,0 +1,9 @@
+# https://leetcode.com/problems/move-zeroes/submissions/
+
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        write_idx=0
+        for i in range(len(nums)):
+            if nums[i]!=0:
+                nums[write_idx], nums[i] = nums[i], nums[write_idx]
+                write_idx+=1
