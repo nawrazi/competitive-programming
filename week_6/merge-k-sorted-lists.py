12d6707222b0b6b2728e9eaa37d8c268a6e4bd20
Nazrawi Demeke <dnazrawi@gmail.com>
2022-02-23 15:33:51 +0300

    more week-6 commits

---
+++ week_6/merge-k-sorted-lists.py
@@ -0,0 +1,22 @@
+# https://leetcode.com/problems/merge-k-sorted-lists/submissions/
+
+class Solution:
+    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+        heap = []
+        curr = start = ListNode()
+        for i in range(len(lists)):
+            if lists[i]:
+                tup = (lists[i].val, i)
+                heapq.heappush(heap,tup)
+
+        while heap:
+            val, idx = heapq.heappop(heap)
+            curr.next = lists[idx]
+
+            curr = curr.next
+            lists[idx] = lists[idx].next
+
+            if lists[idx]:
+                heapq.heappush(heap,(lists[idx].val,idx))
+
+        return start.next
