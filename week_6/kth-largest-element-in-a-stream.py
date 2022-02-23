90a90af3abda90cbc1dcd36679715d1efd3a164e
Nazrawi Demeke <dnazrawi@gmail.com>
2022-02-23 17:38:23 +0300

    more week 6 commits

---
+++ week_6/kth-largest-element-in-a-stream.py
@@ -0,0 +1,23 @@
+class KthLargest:
+
+    def __init__(self, k: int, nums: List[int]):
+        self.k = k
+
+        heapq.heapify(nums)
+        while len(nums)>k:
+            heapq.heappop(nums)
+
+        self.heap = nums
+
+    def add(self, val: int) -> int:
+        if len(self.heap)<self.k:
+            heapq.heappush(self.heap,val)
+            return self.heap[0]
+
+        elif val>self.heap[0] and len(self.heap)==self.k:
+            heapq.heappop(self.heap)
+            heapq.heappush(self.heap,val)
+            return self.heap[0]
+
+        else:
+            return self.heap[0]
