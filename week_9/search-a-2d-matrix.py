531afba55cdbb4d4c97f1093b216b23f9a8a29bd
Nazrawi Demeke <dnazrawi@gmail.com>
2022-02-28 11:14:31 +0300

    first week 9 commit

---
+++ week_9/search-a-2d-matrix.py
@@ -0,0 +1,19 @@
+# https://leetcode.com/problems/search-a-2d-matrix/submissions/
+
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        row_length = len(matrix[0])
+        start, end = 0, (len(matrix)*row_length)-1
+
+        while start<=end:
+            mid = (start+end)//2
+            val = matrix[mid//row_length][mid%row_length]
+
+            if val>target:
+                end = mid-1
+            elif val<target:
+                start = mid+1
+            else:
+                return True
+
+        return False
