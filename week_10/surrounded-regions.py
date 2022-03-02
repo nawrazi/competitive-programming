2af6ec034e1069c2291a8a9d92c38c54a1710030
Nazrawi Demeke <dnazrawi@gmail.com>
2022-03-02 23:01:07 +0300

    more week 10 commits

--- week_10/surrounded-regions.py
+++ week_10/surrounded-regions.py
@@ -25,15 +25,13 @@ class Solution:
         inBound = lambda r,c : 0<=r<len(board) and 0<=c<len(board[0])
 
         for row in range(len(board)):
-            if board[row][0]=='O' and (row,0) not in safe:
-                save(row,0)
-            if board[row][len(board[0])-1]=='O' and (row,len(board[0])-1) not in safe:
-                save(row,len(board[0])-1)
+            for col in (0,len(board[0])-1):
+                if board[row][col]=='O' and (row,col) not in safe:
+                    save(row,col)
 
         for col in range(len(board[0])):
-            if board[0][col]=='O' and (0,col) not in safe:
-                save(0,col)
-            if board[len(board)-1][col]=='O' and (len(board)-1,col) not in safe:
-                save(len(board)-1,col)
+            for row in (0,len(board)-1):
+                if board[row][col]=='O' and (row,col) not in safe:
+                    save(row,col)
 
         captureUnsaved()
