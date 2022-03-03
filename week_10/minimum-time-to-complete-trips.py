5f98b16044a11eafeacaa5a4fb4928ffc58be857
Nazrawi Demeke <dnazrawi@gmail.com>
2022-03-03 23:50:41 +0300

    more week 10 commits

--- week_10/minimum-time-to-complete-trips.py
+++ week_10/minimum-time-to-complete-trips.py
@@ -4,23 +4,23 @@ class Solution:
     def findTime(self, time, n):
         total = 0
         for i in range(len(time)):
-            total += n//time[i]
+            total += n // time[i]
 
         return total
 
 
     def minimumTime(self, time: List[int], totalTrips: int) -> int:
-        start, end = 0, max(time)*totalTrips
+        start, end = 0, max(time) * totalTrips
         best = time[-1]
 
-        while start<=end:
-            mid = (start+end)//2
+        while start <= end:
+            mid = (start + end) // 2
             total = self.findTime(time, mid)
 
             if total >= totalTrips:
                 best = mid
-                end = mid-1
+                end = mid - 1
             else:
-                start = mid+1
+                start = mid + 1
 
         return best
