cf27cd3296b9356c4211c3146b83346fd61672dc
Nazrawi Demeke <dnazrawi@gmail.com>
2022-03-07 12:13:21 +0300

    more week 12 commits

---
+++ week_12/jump-game-iv.py
@@ -0,0 +1,31 @@
+# https://leetcode.com/problems/jump-game-iv/submissions/
+
+class Solution:
+    def minJumps(self, arr: List[int]) -> int:
+        d = {}
+
+        for i, num in enumerate(arr):
+            if num in d:
+                heapq.heappush(d[num], (len(arr) - i, i))
+            else:
+                d[num] = [(len(arr) - i, i)]
+
+        q = deque([(0, 0)])
+        seen = set([0])
+
+        while q:
+            curr, level = q.popleft()
+
+            if curr == len(arr) - 1:
+                return level
+
+            while d[arr[curr]]:
+                _, index = heapq.heappop(d[arr[curr]])
+                if 0 <= index < len(arr) and index not in seen:
+                    seen.add(index)
+                    q.append((index, level+1))
+
+            for index in (curr + 1, curr - 1):
+                if 0 <= index < len(arr) and index not in seen:
+                    seen.add(index)
+                    q.append((index, level+1))
