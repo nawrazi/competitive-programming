0904664785bbe302f91742d8f80c48c01b761dc6
Nazrawi Demeke <dnazrawi@gmail.com>
2022-03-08 15:43:16 +0300

    more week 12 commits

---
+++ week_12/can-place-flowers.py
@@ -0,0 +1,13 @@
+# https://leetcode.com/problems/can-place-flowers/
+
+class Solution:
+    def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
+        flowerbed = [0] + flowerbed + [0]
+        spaces = 0
+
+        for i in range(1, len(flowerbed) - 1):
+            if flowerbed[i] == flowerbed[i-1] == flowerbed[i+1] == 0:
+                flowerbed[i] = 1
+                spaces += 1
+
+        return spaces >= n
