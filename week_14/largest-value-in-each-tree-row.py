10d8d76a8315916c06eb93cca1c578b29ee574d7
Nazrawi Demeke <dnazrawi@gmail.com>
2022-03-24 15:50:51 +0300

    more week 14 commits

---
+++ week_14/largest-value-in-each-tree-row.py
@@ -0,0 +1,22 @@
+# https://leetcode.com/problems/find-largest-value-in-each-tree-row/
+
+class Solution:
+    def largestValues(self, root: Optional[TreeNode]) -> List[int]:
+        q = deque([(root, 0)])
+        level_max = {}
+
+        while q:
+            node, level = q.popleft()
+
+            if not node:
+                continue
+
+            if level in level_max:
+                level_max[level] = max(node.val, level_max[level])
+            else:
+                level_max[level] = node.val
+
+            q.append((node.left, level + 1))
+            q.append((node.right, level + 1))
+
+        return list(level_max.values())
