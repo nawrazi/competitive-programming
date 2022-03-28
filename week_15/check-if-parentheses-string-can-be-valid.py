91d4c2dd105f35c2a6305608b03b2074b806d067
Nazrawi Demeke <dnazrawi@gmail.com>
2022-03-28 15:26:49 +0300

    more week 15 commits

---
+++ week_15/check-if-parentheses-string-can-be-valid.py
@@ -0,0 +1,27 @@
+# https://leetcode.com/problems/check-if-a-parentheses-string-can-be-valid
+
+class Solution:
+    def canBeValid(self, s: str, locked: str) -> bool:
+        if len(s) % 2 != 0:
+            return False
+
+        locked_openers, unlocked_pars = [], []
+
+        for i, par in enumerate(s):
+            if locked[i] == '0':
+                unlocked_pars.append(i)
+            elif par == '(':
+                locked_openers.append(i)
+            else:
+                if len(locked_openers) > 0:
+                    locked_openers.pop()
+                elif len(unlocked_pars) > 0:
+                    unlocked_pars.pop()
+                else:
+                    return False
+
+        while unlocked_pars and locked_openers and unlocked_pars[-1] > locked_openers[-1]:
+            unlocked_pars.pop()
+            locked_openers.pop()
+
+        return not locked_openers
