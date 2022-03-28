c68a58cb8b1da894599d24f5ceea0586556eca5b
Nazrawi Demeke <dnazrawi@gmail.com>
2022-03-28 19:12:39 +0300

    more week 15 commits

--- week_15/min-operations-to-make-array-alternating.py
+++ week_15/min-operations-to-make-array-alternating.py
@@ -2,25 +2,16 @@
 
 class Solution:
     def minimumOperations(self, nums: List[int]) -> int:
-        evens, odds = {}, {}
+        evens, odds = defaultdict(int), defaultdict(int)
 
         for i, num in enumerate(nums):
             if i % 2 == 0:
-                if num in evens:
-                    evens[num] += 1
-                else:
-                    evens[num] = 1
+                evens[num] += 1
             else:
-                if num in odds:
-                    odds[num] += 1
-                else:
-                    odds[num] = 1
+                odds[num] += 1
 
-        oddList = [(num, odds[num]) for num in odds] + [(99, 0)]
-        evenList = [(num, evens[num]) for num in evens] + [(99, 0)]
-
-        oddList.sort(key = lambda x : x[1])
-        evenList.sort(key = lambda x : x[1])
+        oddList = sorted([(n, odds[n]) for n in odds] + [(99,0)], key = lambda x : x[1])
+        evenList = sorted([(n, evens[n]) for n in evens] + [(99,0)], key = lambda x : x[1])
 
         finalEven = evenList[-1][1]
         finalOdd = oddList[-1][1]
