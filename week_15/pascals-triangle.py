9195cfadb0d85dbef714c5baae54d1451175f71d
Nazrawi Demeke <dnazrawi@gmail.com>
2022-03-31 19:19:58 +0300

    more week 15 commits

---
+++ week_15/pascals-triangle.py
@@ -0,0 +1,17 @@
+# https://leetcode.com/problems/pascals-triangle/
+
+class Solution:
+    def generate(self, numRows: int) -> List[List[int]]:
+        if numRows == 1:
+            return [[1]]
+
+        triangle = [[1],[1,1]]
+
+        for i in range(2, numRows):
+            row = [1]
+            for j in range(i - 1):
+                row.append(triangle[i-1][j] + triangle[i-1][j+1])
+
+            triangle.append(row + [1])
+
+        return triangle
