011ed9cdc695ffaa83566414cff96cc6f39124de
Nazrawi Demeke <dnazrawi@gmail.com>
2022-04-05 22:31:12 +0300

    more week 16 commits

--- week_16/maximum-units-on-a-truck.py
+++ week_16/maximum-units-on-a-truck.py
@@ -2,16 +2,11 @@
 
 class Solution:
     def maximumUnits(self, boxes: List[List[int]], truckSize: int) -> int:
-        boxes.sort(key = lambda x: x[1], reverse = True)
+        boxes.sort(key = lambda x : x[1], reverse = True)
         units = 0
-        
-        for i in range(len(boxes)):
-            if boxes[i][0] <= truckSize:
-                units += boxes[i][0] * boxes[i][1]
-                truckSize -= boxes[i][0]
 
-            elif 0 < truckSize < boxes[i][0]:
-                units += truckSize * boxes[i][1]
-                truckSize = 0
+        for i in range(len(boxes)):
+            units += min(boxes[i][0], truckSize) * boxes[i][1]
+            truckSize -= min(boxes[i][0], truckSize)
 
         return units
