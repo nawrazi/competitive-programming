db7cc1b8c5d6a79ac7949302806dac75ea407895
Nazrawi Demeke <dnazrawi@gmail.com>
2022-04-06 00:37:39 +0300

    more week 16 commits

--- week_16/solving-questions-with-brainpower.py
+++ week_16/solving-questions-with-brainpower.py
@@ -3,15 +3,13 @@
 class Solution:
     def mostPoints(self, questions: List[List[int]]) -> int:
         @lru_cache(None)
-        def solve_or_skip(idx):
+        def pick_or_skip(idx):
             if idx >= len(questions):
                 return 0
 
-            pick = questions[idx][0] + solve_or_skip(idx + questions[idx][1] + 1)
-            skip = solve_or_skip(idx + 1)
+            pick = questions[idx][0] + pick_or_skip(idx + questions[idx][1] + 1)
+            skip = pick_or_skip(idx + 1)
 
-            max_score = max(pick, skip)
+            return max(pick, skip)
 
-            return max_score
-
-        return solve_or_skip(0)
+        return pick_or_skip(0)
