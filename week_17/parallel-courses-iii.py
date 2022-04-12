4b9c59ca557389e902ff690243e5da467a3dbfd6
Nazrawi Demeke <dnazrawi@gmail.com>
2022-04-12 21:33:57 +0300

    more week 17 commits

---
+++ week_17/parallel-courses-iii.py
@@ -0,0 +1,25 @@
+# https://leetcode.com/problems/parallel-courses-iii/
+
+class Solution:
+    def minimumTime(self, n: int, relations: List[List[int]], time: List[int]) -> int:
+        @lru_cache(None)
+        def getTime(course):
+            if not graph[course]:
+                return time[course - 1]
+
+            max_time = float(-inf)
+            for crs in graph[course]:
+                max_time = max(getTime(crs), max_time)
+
+            return time[course - 1] + max_time
+
+        graph = defaultdict(list)
+
+        for i, (pre, course) in enumerate(relations):
+            graph[course].append(pre)
+            graph[i + 1]
+        
+        graph[n + 1] = [i + 1 for i in range(n)]
+        time.append(0)
+
+        return getTime(n + 1)
