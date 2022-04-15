8dcd5bec1e94ac42aa3f31a7eda00d1ce1b11984
Nazrawi Demeke <dnazrawi@gmail.com>
2022-04-15 22:13:49 +0300

    more week 18 commits

--- week_18/rotate-array.py
+++ week_18/rotate-array.py
@@ -3,4 +3,4 @@
 class Solution:
     def rotate(self, nums: List[int], k: int) -> None:
         k %= len(nums)
-        nums[:] = nums[len(nums) - k : len(nums)] + nums[0 : len(nums) - k]
+        nums[:] = nums[len(nums) - k :] + nums[: len(nums) - k]
