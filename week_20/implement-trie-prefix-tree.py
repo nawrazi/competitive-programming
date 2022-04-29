7489fb0b3857e8c82b7c74e8dbc19c1693ff9eb1
Nazrawi Demeke <dnazrawi@gmail.com>
2022-04-29 17:44:55 +0300

    more week 20 commits

--- week_20/implement-trie-prefix-tree.py
+++ week_20/implement-trie-prefix-tree.py
@@ -2,40 +2,37 @@
 
 class Node:
     def __init__(self):
-        self.children = [None for _ in range(26)]
+        self.children = {}
         self.final = False
 
 class Trie:
     def __init__(self):
         self.root = Node()
-
+        
     def insert(self, word: str) -> None:
         node = self.root
         for c in word:
-            bit = ord(c) - ord('a')
-            if not node.children[bit]:
-                node.children[bit] = Node()
-            node = node.children[bit]
-
+            if c not in node.children:
+                node.children[c] = Node()
+            node = node.children[c]
+        
         node.final = True
-
+        
     def search(self, word: str) -> bool:
         node = self.root
         for c in word:
-            bit = ord(c) - ord('a')
-            if node.children[bit]:
-                node = node.children[bit]
+            if c in node.children:
+                node = node.children[c]
             else:
                 return False
-
+        
         return node.final
-
+    
     def startsWith(self, prefix: str) -> bool:
         node = self.root
         for c in prefix:
-            bit = ord(c) - ord('a')
-            if node.children[bit]:
-                node = node.children[bit]
+            if c in node.children:
+                node = node.children[c]
             else:
                 return False
 
