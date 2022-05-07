97552766192da3e85e781a6247af04324c340159
Nazrawi Demeke <dnazrawi@gmail.com>
2022-05-07 16:46:36 +0300

    more week 21 commits

---
+++ week_21/implement-strstr.py
@@ -0,0 +1,9 @@
+# https://leetcode.com/problems/implement-strstr/
+
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+        for i in range(len(haystack)):
+            if haystack[i : len(needle) + i] == needle:
+                return i
+            
+        return -1
