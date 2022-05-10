ceef21ea5d7205fb227f142a83b7f9a010cc05b9
Nazrawi Demeke <dnazrawi@gmail.com>
2022-05-10 16:07:10 +0300

    more week 21 commits

--- week_21/summary-ranges.py
+++ week_21/summary-ranges.py
@@ -5,15 +5,10 @@ class Solution:
         final = []
         
         i, j = 0, 1
-        while j < len(nums):
-            diff = nums[j] - nums[j-1]
-            if diff != 1:
-                output = f"{nums[i]}->{nums[j-1]}" if i != j - 1 else f"{nums[i]}"
-                final.append(output)
+        while j < len(nums) + 1:
+            if j == len(nums) or nums[j] - nums[j-1] != 1:
+                final.append(f"{nums[i]}->{nums[j-1]}" if i != j - 1 else f"{nums[i]}")
                 i = j
             j += 1
             
-        if nums:
-            final.append(f"{nums[i]}->{nums[j-1]}" if i != j - 1 else f"{nums[i]}")
-        
         return final
