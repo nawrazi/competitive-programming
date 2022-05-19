caa0a082d899c55aac8d41496b6d00fba839f647
Nazrawi Demeke <dnazrawi@gmail.com>
2022-05-19 17:50:17 +0300

    more week 22 commits

--- week_22/single-number-ii.py
+++ week_22/single-number-ii.py
@@ -9,9 +9,8 @@ public:
         for (int i = 0; i < 32; i++) {
             int count = 0;
             for (int j = 0; j < nums.size(); j++) {
-                if (nums[j] & mask) {
+                if (nums[j] & mask)
                     count++;
-                }
             }
             if (count % 3 == 1)
                 result |= mask;
