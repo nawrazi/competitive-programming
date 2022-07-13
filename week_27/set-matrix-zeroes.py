2dfd02c6c273fce3c390a22382463c8e1f549af1
Nazrawi Demeke <dnazrawi@gmail.com>
2022-07-13 11:22:46 +0300

    more week 27 commits

---
+++ week_27/set-matrix-zeroes.py
@@ -0,0 +1,16 @@
+# https://leetcode.com/problems/set-matrix-zeroes/
+
+class Solution:
+    def setZeroes(self, matrix: List[List[int]]) -> None:\
+        rows = set()
+        cols = set()
+        for r in range(len(matrix)):
+            for c in range(len(matrix[r])):
+                if matrix[r][c] == 0:
+                    rows.add(r)
+                    cols.add(c)
+                    
+        for r in range(len(matrix)):
+            for c in range(len(matrix[r])):
+                if r in rows or c in cols:
+                    matrix[r][c] = 0
