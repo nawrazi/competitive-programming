7d9f8a09514af4248c69ed9ae8f40b1500586576
Nazrawi Demeke <dnazrawi@gmail.com>
2022-07-19 21:16:07 +0300

    more week 28 commits

--- week_28/query-kth-smallest-trimmed-number.py
+++ week_28/query-kth-smallest-trimmed-number.py
@@ -4,7 +4,7 @@ class Solution:
     def smallestTrimmedNumbers(self, nums: List[str], queries: List[List[int]]) -> List[int]:
         data = {}
         for i in range(len(nums[0])):
-            trim = len(nums[0]) - i -1
+            trim = len(nums[0]) - i - 1
             data[i+1] = sorted([(int(num[trim:]), idx) for idx, num in enumerate(nums)])
         
         return [data[trim][k-1][1] for k, trim in queries]
