add74fd29ba61b0a74bceab0622ef0d455befec1
Nazrawi Demeke <dnazrawi@gmail.com>
2022-08-09 22:34:32 +0300

    more week 31 commits

---
+++ week_31/find-peak-element.py
@@ -0,0 +1,16 @@
+# https://leetcode.com/problems/find-peak-element/
+
+class Solution:
+    def findPeakElement(self, nums: List[int]) -> int:
+        start, end = 0, len(nums) - 1
+        
+        while start <= end:
+            mid = (start + end) // 2
+            
+            if mid > 0 and nums[mid] < nums[mid - 1]:
+                end = mid - 1
+            elif mid < len(nums) - 1 and nums[mid] < nums[mid + 1]:
+                start = mid + 1
+            else:
+                return mid
+            
