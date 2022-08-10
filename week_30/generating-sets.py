156ceb3fadff1e6d19cc0d45f66792ee411c6da1
Nazrawi Demeke <dnazrawi@gmail.com>
2022-08-10 21:39:49 +0300

    more week 31 commits

---
+++ week_30/generating-sets.py
@@ -0,0 +1,22 @@
+import heapq
+
+_ = input()
+nums = [int(i) for i in input().split()]
+
+heap = [-num for num in nums]
+heapq.heapify(heap)
+used = set(nums)
+ans = []
+
+while heap:
+    num = -heapq.heappop(heap)
+    cur = num
+    while cur > 0 and cur in used:
+        cur //= 2
+    if cur == 0:
+        ans.append(num)
+    else:
+        used.add(cur)
+        heapq.heappush(heap, -cur)
+
+print(*ans)
