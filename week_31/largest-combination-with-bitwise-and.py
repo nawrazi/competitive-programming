33f77749e60e842c4813465cec1cdb446d97807b
Nazrawi Demeke <dnazrawi@gmail.com>
2022-08-11 15:42:34 +0300

    more week 31 commits

---
+++ week_31/largest-combination-with-bitwise-and.py
@@ -0,0 +1,13 @@
+# https://leetcode.com/problems/largest-combination-with-bitwise-and-greater-than-zero/
+
+class Solution:
+    def largestCombination(self, candidates: List[int]) -> int:
+        counts = defaultdict(int)
+        for num in candidates:
+            mask = 1
+            for _ in range(32):
+                if num & mask != 0:
+                    counts[mask] += 1
+                mask <<= 1
+        return max(counts.values())
+    
