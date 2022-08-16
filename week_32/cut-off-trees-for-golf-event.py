cc477ee5b20fdd3abd1728d783ffc772f1a09429
Nazrawi Demeke <dnazrawi@gmail.com>
2022-08-16 20:56:20 +0300

    more week 32 commits

---
+++ week_32/cut-off-trees-for-golf-event.py
@@ -0,0 +1,36 @@
+# https://leetcode.com/problems/cut-off-trees-for-golf-event/
+# https://leetcode.com/problems/cut-off-trees-for-golf-event/solution/
+
+class Solution:
+    def findDistance(self, forest, sr, sc, tr, tc):
+        R, C = len(forest), len(forest[0])
+        processed = set()
+        deque = collections.deque([(0, sr, sc)])
+        while deque:
+            detours, r, c = deque.popleft()
+            if (r, c) not in processed:
+                processed.add((r, c))
+                if r == tr and c == tc:
+                    return abs(sr-tr) + abs(sc-tc) + 2*detours
+                
+                for nr, nc, closer in ((r-1, c, r > tr), (r+1, c, r < tr), (r, c-1, c > tc), (r, c+1, c < tc)):
+                    if 0 <= nr < R and 0 <= nc < C and forest[nr][nc]:
+                        if closer:
+                            deque.appendleft((detours, nr, nc))
+                        else:
+                            deque.append((detours+1, nr, nc))
+                            
+        return -1
+    
+    def cutOffTree(self, forest):
+        trees = sorted((v, r, c) for r, row in enumerate(forest) for c, v in enumerate(row) if v > 1)
+        sr = sc = ans = 0
+        for _, tr, tc in trees:
+            d = self.findDistance(forest, sr, sc, tr, tc)
+            if d < 0:
+                return -1
+            ans += d
+            sr, sc = tr, tc
+            
+        return ans
+    
