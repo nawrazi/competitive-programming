1ef94847f00c4a59a9b3218d1feb8baa34af63ef
Nazrawi Demeke <dnazrawi@gmail.com>
2022-08-17 17:37:54 +0300

    more week 32 commits

--- week_32/valid-square.py
+++ week_32/valid-square.py
@@ -7,14 +7,9 @@ class Solution:
         
         for x1, y1 in points:
             for x2, y2 in points:
-                if (x1, y1) == (x2, y2):
-                    continue
-                
-                side = ((x1 - x2) ** 2) + ((y1 - y2) ** 2)
-                sides.add(side)
-                
-                slope = (y2 - y1) / (x1 - x2) if x1 != x2 else inf
-                slopes.add(slope)
+                if (x1, y1) != (x2, y2):
+                    sides.add(((x1 - x2) ** 2) + ((y1 - y2) ** 2))
+                    slopes.add((y2 - y1) / (x1 - x2) if x1 != x2 else inf)
                 
         return len(sides) == 2 and len(slopes) == 4
     
