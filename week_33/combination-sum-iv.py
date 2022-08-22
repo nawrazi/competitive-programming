3a6c08070069da71bee779d1d25901d0b3588880
Nazrawi Demeke <dnazrawi@gmail.com>
2022-08-22 21:15:06 +0300

    more week 33 commits

---
+++ week_33/combination-sum-iv.py
@@ -0,0 +1,21 @@
+# https://leetcode.com/problems/combination-sum-iv/
+
+class Solution:
+    def combinationSum4(self, nums: List[int], target: int) -> int:
+        
+        @cache
+        def getCombinations(needed):
+            if needed == 0:
+                return 1
+            
+            combos = 0
+            for i in range(len(nums)):
+                if nums[i] > needed:
+                    continue
+                combos += getCombinations(needed - nums[i])
+                    
+            return combos
+        
+        
+        return getCombinations(target)
+    
