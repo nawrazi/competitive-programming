dee282a444aa19ba1605ea36fe4533e49f8f0dfb
Nazrawi Demeke <dnazrawi@gmail.com>
2022-08-28 20:07:38 +0300

    first week 34 commit

--- week_34/sort-the-matrix-diagonally.py
+++ week_34/sort-the-matrix-diagonally.py
@@ -3,27 +3,18 @@
 class Solution:
     def diagonalSort(self, mat: List[List[int]]) -> List[List[int]]:
         m, n = len(mat), len(mat[0])
-        inBound = lambda r, c: 0 <= r < m and 0 <= c < n
+        diags = defaultdict(list)
+        for r, row in enumerate(mat):
+            for c, cell in enumerate(row):
+                diags[r - c].append(cell)
         
-        def search(row, col, place, idx):
-            if not inBound(row, col):
-                return
-            
-            if place:
-                ans[row][col] = current[idx]
-                search(row + 1, col + 1, place, idx + 1)
-            else:
-                current.append(mat[row][col])
-                search(row + 1, col + 1, place, idx)
-                
+        for diag in diags.values():
+            diag.sort(reverse = True)
+        
         ans = [[0 for _ in range(n)] for _ in range(m)]
-        starts = [(0, c) for c in range(n)] + [(r, 0) for r in range(1, m)]
-        
-        for r, c in starts:
-            current = []
-            search(r, c, False, 0)
-            current.sort()
-            search(r, c, True, 0)
-            
+        for r, row in enumerate(mat):
+            for c, cell in enumerate(row):
+                ans[r][c] = diags[r - c].pop()
+                
         return ans
     
