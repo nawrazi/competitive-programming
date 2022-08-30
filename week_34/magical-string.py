9a983a0ca20ffd95e234565c25fbeb2a31f0d096
Nazrawi Demeke <dnazrawi@gmail.com>
2022-08-30 21:49:26 +0300

    more week 34 commits

--- week_34/magical-string.py
+++ week_34/magical-string.py
@@ -4,12 +4,12 @@ class Solution:
     def magicalString(self, n: int) -> int:
         opp = {'1': '2', '2': '1'}
         s = '122'
-        cur = '2'
+        cur = '1'
         size = 3
         i = 2
         
         while size < n:
-            s += opp[cur] * int(s[i])
+            s += cur * int(s[i])
             cur = opp[cur]
             size += int(s[i])
             i += 1
