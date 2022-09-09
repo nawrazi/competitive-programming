c3250536e6a5483655cccc6c501d0e583a802b98
Nazrawi Demeke <dnazrawi@gmail.com>
2022-09-09 11:49:18 +0300

    more week 35 commits

--- week_35/number-of-weak-characters-in-game.py
+++ week_35/number-of-weak-characters-in-game.py
@@ -3,18 +3,15 @@
 class Solution:
     def numberOfWeakCharacters(self, properties: List[List[int]]) -> int:
         eq_attack = defaultdict(list)
-        
-        for a, d in properties:
+        for a, d in sorted(properties):
             eq_attack[a].append(d)
-        
+            
         props = []
-        for a, eqs in eq_attack.items():
-            eqs.sort()
+        for a, eqs in sorted(eq_attack.items()):
             props.append([a, eqs[-1]])
-        
-        cur_max = -inf
-        weak = 0
-        for a, d in sorted(props, reverse=True):
+            
+        weak, cur_max = 0, 0
+        for a, d in reversed(props):
             weak += bisect_left(eq_attack[a], cur_max)
             cur_max = max(cur_max, eq_attack[a][-1])
             
