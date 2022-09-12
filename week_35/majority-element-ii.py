077e7d41192661e624be05a75f8958acde6d3e19
Nazrawi Demeke <dnazrawi@gmail.com>
2022-09-12 16:43:59 +0300

    more week 35 commits

---
+++ week_35/majority-element-ii.py
@@ -0,0 +1,6 @@
+# https://leetcode.com/problems/majority-element-ii/
+
+class Solution:
+    def majorityElement(self, nums: List[int]) -> List[int]:
+        return [n for n, c in Counter(nums).items() if c > len(nums) / 3]
+    
