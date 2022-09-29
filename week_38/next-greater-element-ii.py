04a63fe9dbf400a8cfdaece87facb3f6edec2258
Nazrawi Demeke <dnazrawi@gmail.com>
2022-09-29 11:23:22 +0300

    more week 38 commits

---
+++ week_38/next-greater-element-ii.py
@@ -0,0 +1,15 @@
+# https://leetcode.com/problems/next-greater-element-ii/
+
+class Solution:
+    def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        d = {}
+        mono_stack = []
+        
+        for num in nums2:
+            while mono_stack and num>mono_stack[-1]:
+                d[mono_stack.pop()] = num
+                
+            mono_stack.append(num)
+            
+        return [(d[num] if num in d.keys() else -1) for num in nums1]
+    
