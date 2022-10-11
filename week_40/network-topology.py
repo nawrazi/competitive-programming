863727220818b5a207bb7aa05830a97307e94331
Nazrawi Demeke <dnazrawi@gmail.com>
2022-10-11 11:58:11 +0300

    more week 40 commits

---
+++ week_40/network-topology.py
@@ -0,0 +1,33 @@
+# https://codeforces.com/gym/403303/problem/C
+
+from collections import defaultdict
+
+n, m = [int(n) for n in input().split()]
+graph = defaultdict(set)
+dep = defaultdict(int)
+
+for _ in range(m):
+    a, b = [int(n) for n in input().split()]
+    graph[a].add(b)
+    graph[b].add(a)
+    dep[a] += 1
+    dep[b] += 1
+
+ones, twos, most = 0, 0, 0
+for i, d in dep.items():
+    if d == 1:
+        ones += 1
+    if d == 2:
+        twos += 1
+    most = max(most, d)
+
+path = set()
+if ones == 2 and twos == n - 2:
+    print('bus topology')
+elif twos == n:
+    print('ring topology')
+elif ones == n - 1 and most == n - 1:
+    print('star topology')
+else:
+    print('unknown topology')
+    
