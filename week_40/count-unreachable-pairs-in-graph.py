793fc257b516d86137b09491fc4405c599041b7c
Nazrawi Demeke <dnazrawi@gmail.com>
2022-10-12 15:37:50 +0300

    more week 40 commits

---
+++ week_40/count-unreachable-pairs-in-graph.py
@@ -0,0 +1,32 @@
+# https://leetcode.com/problems/count-unreachable-pairs-of-nodes-in-an-undirected-graph/
+
+class Solution:
+    def countPairs(self, n: int, edges: List[List[int]]) -> int:
+        parents = [-1 for _ in range(n)]
+        
+        def find(node):
+            if parents[node] < 0:
+                return node
+            parents[node] = find(parents[node])
+            return parents[node]
+        
+        def union(node1, node2):
+            parent1, parent2 = find(node1), find(node2)
+            
+            if parents[parent1] > parents[parent2]:
+                parent1, parent2 = parent2, parent1
+                
+            if parent1 != parent2:
+                parents[parent1] += parents[parent2]
+                parents[parent2] = parent1
+        
+        for left, right in edges:
+            union(left, right)
+            
+        total = 0
+        for par in parents:
+            if par < 0:
+                total += -par * (n + par)
+                
+        return total // 2
+    
