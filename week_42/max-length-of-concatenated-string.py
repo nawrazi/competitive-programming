568f665db178e46d7b5a2809b7ae0f85736821d2
Nazrawi Demeke <dnazrawi@gmail.com>
2022-10-24 14:28:39 +0300

    more week 41 commits

---
+++ week_42/max-length-of-concatenated-string.py
@@ -0,0 +1,32 @@
+# https://leetcode.com/problems/maximum-length-of-a-concatenated-string-with-unique-characters/
+
+class Solution:
+    def maxLength(self, arr: List[str]) -> int:
+        chars = set()
+        self.max_len = 0
+        
+        def concatenate(idx):
+            if idx >= len(arr):
+                self.max_len = max(self.max_len, len(chars))
+                return
+            
+            can_pick = True
+            for c in arr[idx]:
+                if c in chars:
+                    can_pick = False
+                    break
+                    
+            # use current word
+            if can_pick and len(set(arr[idx])) == len(arr[idx]):
+                for c in arr[idx]:
+                    chars.add(c)
+                concatenate(idx + 1)
+                for c in arr[idx]:
+                    chars.remove(c)
+                
+            # dont use current word
+            concatenate(idx + 1)
+            
+        concatenate(0)
+        return self.max_len
+    
