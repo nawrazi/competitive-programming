ca2c5f1028fadb1d8f0769afd032da044de32c28
Nazrawi Demeke <dnazrawi@gmail.com>
2022-11-02 15:57:08 +0300

    more week 43 commits

--- week_43/min-number-of-frogs-croaking.py
+++ week_43/min-number-of-frogs-croaking.py
@@ -4,6 +4,7 @@ class Solution:
     def minNumberOfFrogs(self, croakOfFrogs: str) -> int:
         count = {'x': inf, 'c': 0, 'r': 0, 'o': 0, 'a': 0, 'k': 0}
         prev = {'c': 'x', 'r': 'c', 'o': 'r', 'a': 'o', 'k': 'a'}
+        diff = {'c': 1, 'k': -1}
         min_frogs = 0
         cur_frogs = 0
         
@@ -11,12 +12,8 @@ class Solution:
             if char not in count or count[char] >= count[prev[char]]:
                 return -1
             
-            if char == 'c':
-                cur_frogs += 1
-            elif char == 'k':
-                cur_frogs -= 1
-                
             count[char] += 1
+            cur_frogs += diff.get(char, 0)
             min_frogs = max(min_frogs, cur_frogs)
                 
         return min_frogs if count['c'] == count['k'] else -1
