6b37678c38ce8d30dad9fa5a01b0f605a01c23b1
Nazrawi Demeke <dnazrawi@gmail.com>
2022-11-09 10:26:55 +0300

    more week 44 commits

---
+++ week_44/find-original-array-of-prefix-xor.py
@@ -0,0 +1,10 @@
+# https://leetcode.com/problems/find-the-original-array-of-prefix-xor/
+
+class Solution:
+    def findArray(self, pref: List[int]) -> List[int]:
+        arr = [pref[0]]
+        for i in range(1, len(pref)):
+            arr.append(pref[i] ^ pref[i - 1])
+            
+        return arr
+    
