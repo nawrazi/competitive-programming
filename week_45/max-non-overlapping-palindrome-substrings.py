e0335034ad64821de1eb5ef77c31749a717e433b
Nazrawi Demeke <dnazrawi@gmail.com>
2022-11-13 14:08:36 +0300

    more week 45 commits

--- week_45/max-non-overlapping-palindrome-substrings.py
+++ week_45/max-non-overlapping-palindrome-substrings.py
@@ -2,39 +2,35 @@
 
 class Solution:
     def maxPalindromes(self, s: str, k: int) -> int:
-        if k == 1:
-            return len(s)
-        
         def getPalindrome(idx, single):
             i, j = idx - 1, idx + 1
-            center = [s[idx]]
-            side = []
+            length = 1
             
             if not single:
-                center.append(s[j])
+                length += 1
                 j += 1
                 
             while i >= 0 and j < len(s) and s[i] == s[j]:
-                side.append(s[i])
+                length += 2
                 i -= 1
                 j += 1
                 
-            return 2 * len(side) + len(center)
+            return length
         
-        pals = []
+        palindromes = []
         for i in range(len(s)):
             length = getPalindrome(i, True)
             if length >= k:
-                pals.append([i - (k // 2), i + (k // 2)])
+                palindromes.append([i - (k // 2), i + (k // 2)])
                 
-            if i < len(s) - 1 and s[i] == s[i + 1]:
+            if k > 1 and i < len(s) - 1 and s[i] == s[i + 1]:
                 length = getPalindrome(i, False)
                 if length >= k:
-                    pals.append([i - ((k // 2) - 1), i + (k // 2)])
+                    palindromes.append([i - ((k // 2) - 1), i + (k // 2)])
                     
         last = -inf
         total = 0
-        for start, end in sorted(pals, key = lambda p: p[1]):
+        for start, end in sorted(palindromes, key = lambda p: p[1]):
             if start > last: 
                 last = end
                 total += 1
