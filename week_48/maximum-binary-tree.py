155a10559ff6fc6f73caef94f676b64bc1271fb2
Nazrawi Demeke <dnazrawi@gmail.com>
2022-12-08 14:38:06 +0300

    more week 48 commits

---
+++ week_48/maximum-binary-tree.py
@@ -0,0 +1,23 @@
+# https://leetcode.com/problems/maximum-binary-tree/description/
+
+class Solution:
+    def constructMaximumBinaryTree(self, nums: List[int]) -> Optional[TreeNode]:
+        def construct(left, right):
+            if left == right:
+                return TreeNode(nums[left])
+            
+            if left > right:
+                return None
+            
+            best = [-inf, 0]
+            for idx in range(left, right + 1):
+                best = max(best, [nums[idx], idx])
+                
+            return TreeNode(
+                val = best[0],
+                left = construct(left, best[1] - 1),
+                right = construct(best[1] + 1, right)
+            )
+        
+        return construct(0, len(nums) - 1)
+    
