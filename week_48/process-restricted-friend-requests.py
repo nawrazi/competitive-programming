4982cd8bea90e06e85bd5518ac499b77e2151603
Nazrawi Demeke <dnazrawi@gmail.com>
2022-12-10 15:00:11 +0300

    more week 48 commits

--- week_48/process-restricted-friend-requests.py
+++ week_48/process-restricted-friend-requests.py
@@ -3,13 +3,13 @@
 class Solution:
     def friendRequests(self, n: int, restrictions: List[List[int]], requests: List[List[int]]) -> List[bool]:
         parents = [-1 for _ in range(n)]
-
+        
         def find(node):
             if parents[node] < 0:
                 return node
             parents[node] = find(parents[node])
             return parents[node]
-
+        
         def union(node1, node2):
             nonlocal parents
             parent1, parent2 = find(node1), find(node2)
@@ -17,17 +17,17 @@ class Solution:
             
             if parents[parent1] > parents[parent2]:
                 parent1, parent2 = parent2, parent1
-
+                
             if parent1 != parent2:
                 parents[parent1] += parents[parent2]
                 parents[parent2] = parent1
-
+                
             for u, v in restrictions:
                 if find(u) == find(v):
                     parents = backup
                     return False
-
+                
             return True
-
+        
         return [union(u, v) for u, v in requests]
     
