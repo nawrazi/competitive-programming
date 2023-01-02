e52758a353cd0f971e2c4907a9e1b3ef4e55c253
Nazrawi Demeke <dnazrawi@gmail.com>
2023-01-02 13:21:59 +0300

    more week 51 commits

--- week_51/longest-arithmetic-subsequence-of-given-diff.py
+++ week_51/longest-arithmetic-subsequence-of-given-diff.py
@@ -3,8 +3,8 @@
 class Solution:
     def longestSubsequence(self, arr: List[int], difference: int) -> int:
         dp = {}
-        for i in range(len(arr) - 1, -1, -1):
-            dp[arr[i]] = 1 + dp.get(arr[i] + difference, 0)
+        for num in reversed(arr):
+            dp[num] = 1 + dp.get(num + difference, 0)
             
         return max(dp.values())
     
