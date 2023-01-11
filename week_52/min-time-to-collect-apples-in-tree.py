fb9ea30c621836f6f04f2ef488acdf49c734dc14
Nazrawi Demeke <dnazrawi@gmail.com>
2023-01-11 14:13:39 +0300

    more week 52 commits

---
+++ week_52/min-time-to-collect-apples-in-tree.py
@@ -0,0 +1,26 @@
+# https://leetcode.com/problems/minimum-time-to-collect-all-apples-in-a-tree/description/
+
+class Solution:
+    def minTime(self, n: int, edges: List[List[int]], hasApple: List[bool]) -> int:
+        graph = [[] for _ in range(n)]
+        for u, v in edges:
+            graph[u].append(v)
+            graph[v].append(u)
+            
+        def getTime(node, parent):
+            if node is None:
+                return 0
+            
+            time = 0
+            for child in graph[node]:
+                if child != parent:
+                    time += getTime(child, node)
+                    
+            if time == 0:
+                return hasApple[node]
+            
+            return time + 1
+        
+        time = getTime(0, -1)
+        return 2 * (time - 1) if time != 0 else 0
+    
