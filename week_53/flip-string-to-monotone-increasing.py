7c135b04a9a9755236b0396eb19716c1fe47fc0d
Nazrawi Demeke <dnazrawi@gmail.com>
2023-01-17 12:04:28 +0300

    more week 53 commits

--- week_53/flip-string-to-monotone-increasing.py
+++ week_53/flip-string-to-monotone-increasing.py
@@ -2,16 +2,12 @@
 
 class Solution:
     def minFlipsMonoIncr(self, s: str) -> int:
-        ones = 0
-        zeros = s.count('0')
-        flips = min(s.count('1'), s.count('0'))
+        min_flips = s.count('1')
+        flips = s.count('0')
         
         for idx in range(len(s)):
-            flips = min(flips, zeros + ones)
-            if s[idx] == '1':
-                ones += 1
-            else:
-                zeros -= 1
-                
-        return flips
+            min_flips = min(min_flips, flips)
+            flips += 1 if s[idx] == '1' else -1
+            
+        return min_flips
     
