882e634fbc0ed9dba4779cc901fef312f89b7fba
Nazrawi Demeke <dnazrawi@gmail.com>
2023-02-02 11:05:37 +0300

    more week 55 commits

--- week_55/integer-to-english-words.py
+++ week_55/integer-to-english-words.py
@@ -7,34 +7,34 @@ class Solution:
         teen = ['Ten', 'Eleven', 'Twelve', 'Thirteen', 'Fourteen', 'Fifteen', 'Sixteen', 'Seventeen', 'Eighteen', 'Nineteen']
         place = ['', 'Thousand', 'Million', 'Billion']
         
-        def toWord(num, power):
-            word = []
+        def convert(num, power):
+            words = []
             
             if num[0] != '0':
-                word.append(ones[int(num[0])])
-                word.append('Hundred')
+                words.append(ones[int(num[0])])
+                words.append('Hundred')
                 
             if num[1] == '1':
-                word.append(teen[int(num[1:]) - 10])
+                words.append(teen[int(num[1:]) - 10])
             else:
-                word.append(tens[int(num[1])])
-                word.append(ones[int(num[2])])
+                words.append(tens[int(num[1])])
+                words.append(ones[int(num[2])])
                 
             if int(num) != 0:
-                word.append(place[power])
+                words.append(place[power])
                 
-            return ' '.join(w for w in word if w)
+            return ' '.join(w for w in words if w)
         
         if num == 0:
             return 'Zero'
         
         num = ('0' * (3 - (len(str(num)) % 3))) + str(num)
-        word = []
         power = (len(num) // 3) - 1
+        words = []
         
         for idx in range(0, len(num), 3):
-            word.append(toWord(num[idx:idx+3], power))
+            words.append(convert(num[idx:idx+3], power))
             power -= 1
             
-        return ' '.join(w for w in word if w)
+        return ' '.join(w for w in words if w)
     
