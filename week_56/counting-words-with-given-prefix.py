12298a3b1d2e1c3c5bf8ab224af7b9d3dea8cc3d
Nazrawi Demeke <dnazrawi@gmail.com>
2023-02-06 17:08:02 +0300

    more week 56 commits

---
+++ week_56/counting-words-with-given-prefix.py
@@ -0,0 +1,11 @@
+# https://leetcode.com/problems/counting-words-with-a-given-prefix/description/
+
+class Solution:
+    def prefixCount(self, words: List[str], pref: str) -> int:
+        count = 0
+        for word in words:
+            if word.startswith(pref):
+                count += 1
+                
+        return count
+    
