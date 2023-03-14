a7d4ed6a39cd1eb0cb156c2a0a1328616e2ca13e
Nazrawi Demeke <dnazrawi@gmail.com>
2023-03-14 08:26:43 +0300

    more week 59 commits

---
+++ week_59/flip-equivalent-binary-trees.py
@@ -0,0 +1,13 @@
+# https://leetcode.com/problems/flip-equivalent-binary-trees/
+
+class Solution:
+    def flipEquiv(self, root1: Optional[TreeNode], root2: Optional[TreeNode]) -> bool:
+        if not root1 and not root2:
+            return True
+        if not root1 or not root2 or root1.val != root2.val:
+            return False
+        
+        sim = self.flipEquiv(root1.left, root2.left) and self.flipEquiv(root1.right, root2.right)
+        opp = self.flipEquiv(root1.left, root2.right) and self.flipEquiv(root1.right, root2.left)
+        return sim or opp
+    
