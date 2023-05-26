2e1e0fe67c5ec46b090a2199350d9e8935e5e5d9
Nazrawi Demeke <dnazrawi@gmail.com>
2023-05-26 16:14:08 +0300

    Create find-longest-valid-obstacle-course-at-each-position.py

---
+++ week_65/find-longest-valid-obstacle-course-at-each-position.py
@@ -0,0 +1,15 @@
+# https://leetcode.com/problems/find-the-longest-valid-obstacle-course-at-each-position/
+
+class Solution:
+    def longestObstacleCourseAtEachPosition(self, obstacles: List[int]) -> List[int]:
+        dp = []
+        for num in obstacles:
+            pos = bisect_right(dp, num)
+            
+            if pos == len(dp):
+                dp.append(num)
+            else:
+                dp[pos] = num
+                
+            yield pos + 1
+        
